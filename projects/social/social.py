edc40a9dc91ad45e5994bacafa4d8fa6a721eae9
Nathan <nate1898@gmail.com>
2020-11-19 16:48:47 -0500

    Social Mvp

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,4 @@
+import random
 class User:
     def __init__(self, name):
         self.name = name
@@ -43,11 +44,24 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
+        if(num_users < avg_friendships):
+            return None
         # Add users
-
+        for i in range(num_users):
+            self.add_user(i)
         # Create friendships
-
+        userlist = list(self.users.keys())
+        for i in range(1, num_users):
+            userlist.remove(i)
+            random.shuffle(userlist)
+            count = random.randint(0,avg_friendships * 2 - 1)
+            if count == 0:
+                userlist.append(i)
+            else:
+                for j in range(0, count):
+                    if random.getrandbits(1):
+                        self.add_friendship(i, userlist[j]) 
+                userlist.append(i)
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -59,12 +73,40 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        for i in range(1, self.last_id + 1):
+            if i == user_id:
+                visited[user_id] = i
+            else:
+                l = bfs(self.friendships, user_id, i)
+                if l:
+                    visited[i] = l
         return visited
+
+def bfs(graph, starting_vertex, destination_vertex):
+        explored = []
+
+        queue = [[starting_vertex]]
+
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+
+            if node not in explored:
+                neighbors = graph[node]
+
+                for neighbor in neighbors:
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    queue.append(new_path)
+
+                    if neighbor == destination_vertex:
+                        return new_path
+                explored.append(node)
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(1000, 5)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
